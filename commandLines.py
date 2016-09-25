49c14de66faa74381c7ed9bd8fc5df69ca7c01ff
kippda <dalexanderkipp@gmail.com>
2016-09-25 16:16:40 -0500

    Update commandLines.py

--- commandLines.py
+++ commandLines.py
@@ -4,20 +4,28 @@
 
 def hasUppercase(string):
     """
-    Checks string and returns True if at least one letter is uppercase
-    Otherwise returns False
+    Checks string and returns True if at least one letter
+    is uppercase. Otherwise returns False
     """
     for ch in string:
         if ch.isupper():
             return True
     return(False)
 
-#has an even digit
+def hasEvenDigit(string):
+    """
+    Checks string and returns True if at least one character
+    is an even number. Otherwise returns False
+    """
+    for ch in string:
+        if ch.isdigit() and int(ch) % 2 == 0:
+            return(True)
+    return(False)
 
 def endsWithS(string):
     """
     Checks string and returns True if it ends with 's' or 'S'
-    False
+    Otherwise returns False
     """
     if string[-1].lower() == 's':
         return(True)
