579c6c54257e0726af9eca1e2244439a33e69c45
kippda <dalexanderkipp@gmail.com>
2016-09-26 22:39:19 -0500

    Update commandLines.py

--- commandLines.py
+++ commandLines.py
@@ -1,16 +1,27 @@
-#Valid SSN
+def isValidSSN(string):
+    """
+    Checks string and returns True if it's a valid
+    Social Security Number. Otherwise returns false
+    """
+    ssnDigits = ""
+    if len(string) == 11:
+        ssnDigits = string[:3]+string[4:6]+string[7:]
+        if ssnDigits.isdigit() and string[3:7:3] == "--":
+            return True
+    return False
+
 
 def isPalindrome(string):
     """
-    Checks string and returns True is it's a palindrome
+    Checks string and returns True if it's a palindrome
     Otherwise returns False
     """
     tmpList = list(string)
     tmpList.reverse()
     backwards = ''.join(tmpList)
     if string == backwards:
-        return(True)
-    return(False)
+        return True
+    return False 
     
     
 def hasUppercase(string):
@@ -21,7 +32,7 @@ def hasUppercase(string):
     for ch in string:
         if ch.isupper():
             return True
-    return(False)
+    return False
 
 def hasEvenDigit(string):
     """
@@ -30,8 +41,8 @@ def hasEvenDigit(string):
     """
     for ch in string:
         if ch.isdigit() and int(ch) % 2 == 0:
-            return(True)
-    return(False)
+            return True
+    return False
 
 def endsWithS(string):
     """
@@ -39,8 +50,8 @@ def endsWithS(string):
     Otherwise returns False
     """
     if string[-1].lower() == 's':
-        return(True)
-    return(False)
+        return True
+    return False
 
 #create empty list for each thing we're checking
 ssnLst = []
@@ -53,7 +64,8 @@ sList = []
 lstOfStrings = ["111-22-3333", "racecar", "Hello", "1111", "12", "121", "racecars", "S"]
 
 for string in lstOfStrings:
-    #SSN
+    if isValidSSN(string) is True:
+        ssnLst.append(string)
     if isPalindrome(string) is True:
         palinLst.append(string)
     if hasUppercase(string) is True:
@@ -63,7 +75,7 @@ for string in lstOfStrings:
     if endsWithS(string) is True:
         sList.append(string)
 
-#print(ssnLst)
+print(ssnLst)
 print(ssnLst)
 print(palinLst)
 print(upCaseLst)
